e47ee168f05c3c0ed6f718fc3840c017a98da37f
mrmiywj <jsyangwenjie@gmail.com>
2015-07-28 11:29:13 +0000

    add makefile test cleantest

--- UMD/2013/p3/test.ml
+++ UMD/2013/p3/test.ml
@@ -1,10 +1,13 @@
 open OUnit2
+open Ast
 
 let test1 test_ctxt = assert_equal 1 (Rube.fstoffour (1,2,3,4))
+let test2 test_ctxt = assert_equal true (Rube.defined_class {prog_clss=[{cls_name="class";cls_super="Object";cls_fields=[("",TClass "")];cls_meths=[]}];prog_main=ENil} "class") 
 
 let suit =
   "suite">:::
-    ["test1" >:: test1]
+    ["test1" >:: test1;
+    "test2" >:: test2]
 ;;
 
 let () =
